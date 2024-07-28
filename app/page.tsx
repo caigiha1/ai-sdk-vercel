74f8b50c720d109f6b1cda6cf397cc2a709ed409
william.tran <william.tran@techvify.com.vn>
2024-07-28 21:38:14 +0700

    feat: update

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,41 @@
+"use client";
+
 import ChatList from "@/components/chat-list";
 import ChatScrollAnchor from "@/components/chat-scroll-anchor";
-import { env } from "@/env";
+import { useEnterSubmit } from "@/lib/use-enter-submit";
+import { useForm } from "react-hook-form";
+import TextareaAutosize from "react-textarea-autosize";
 
 export default function Home() {
+	const form = useForm();
+	const { formRef, onKeyDown } = useEnterSubmit();
+	const onSubmit = (data: any) => {
+		console.log(data);
+	};
+
 	return (
 		<main>
 			<div className="pb-[200px] pt-4 md:pt-10">
 				<ChatList messages={[]} />
 				<ChatScrollAnchor />
+			</div>
+			<div className="">
+				<form ref={formRef} onSubmit={form.handleSubmit(onSubmit)} action="" />
+				<div className="relative flex flex-col w-full overflow-hidden max-h-60 grow bg-background">
+					<TextareaAutosize
+						tabIndex={0}
+						onKeyDown={onKeyDown}
+						placeholder="send a message."
+						className="min-h-[60px] w-full resize-none bg-transparent pl-4 pr-16 py-[1.3rem] focus-within:outline-none sm:text-sm"
+						autoFocus
+						spellCheck={false}
+						autoComplete="off"
+						autoCorrect="off"
+						rows={1}
+						{...form.register("message")}
+					/>
+					<h1>Hello this is a chat box</h1>
+				</div>
 			</div>
 		</main>
 	);
--- components/chat-scroll-anchor.tsx
+++ components/chat-scroll-anchor.tsx
@@ -1,5 +1,25 @@
 "use client";
 
-export default function ChatScrollAnchor() {
-	return <div>hello</div>;
-}
+import useAtBottom from "@/lib/use-at-bottom";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+
+const ChatScrollAnchor = () => {
+	const trackVisibility = true;
+	const isAtBottom = useAtBottom();
+	const { ref, entry, inView } = useInView({
+		trackVisibility: true,
+		delay: 100,
+		rootMargin: "0px 0px -50px 0px",
+	});
+
+	useEffect(() => {
+		if (isAtBottom && trackVisibility && !inView) {
+			entry?.target.scrollIntoView({ block: "start" });
+		}
+	}, [isAtBottom, inView, entry]);
+
+	return <div ref={ref} className="h-px w-full" />;
+};
+
+export default ChatScrollAnchor;
